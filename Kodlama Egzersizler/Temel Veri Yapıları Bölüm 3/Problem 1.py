69cc1001f10db4e05424ad921483b8ce0f63aa9d
Hızır Kaan Erkan <seninmailin@gmail.com>
2025-05-13 23:06:54 +0300

    Değişiklikler kaydedildi (cleanup sonrası)

---
+++ Kodlama Egzersizler/Temel Veri Yapıları Bölüm 3/Problem 1.py
@@ -0,0 +1,10 @@
+print("Üçlü Çarpım Programı Şeysi")
+
+a = int(input("Birinci Sayıyı Giriniz : "))
+b = int(input("İkinci Sayıyı Giriniz : "))
+c = int(input("Üçüncü Sayıyı Giriniz : "))
+
+print("Birinci Sayı : {}\nİkinci Sayı : {}\nÜçüncü Sayı : {}".format(a,b,c))
+
+sonucunuz = a * b * c
+print("Sonucunuz",sonucunuz)
---
+++ Kodlama Egzersizler/Temel Veri Yapıları Bölüm 3/Problem 2.py
@@ -0,0 +1,9 @@
+print("Beden İndeksine Hoş Geldiniz")
+print("Lütfen aşağıya beden değerlerinizi giriniz...")
+
+a = int(input("Kilo : "))
+b = (float(input("Boy : ")))
+
+endex = a / b ** 2
+
+print("Beden Kitle İndeksiniz : {}".format(endex))
