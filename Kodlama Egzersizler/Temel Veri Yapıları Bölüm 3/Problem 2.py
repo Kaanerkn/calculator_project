522b15e4e7bca35a376f46b2091826ec821c821a
Hızır Kaan Erkan <seninmailin@gmail.com>
2025-05-13 23:09:05 +0300

    Gereksiz egzersiz dosyaları kaldırıldı

--- Kodlama Egzersizler/Temel Veri Yapıları Bölüm 3/Problem 2.py
+++
@@ -1,9 +0,0 @@
-print("Beden İndeksine Hoş Geldiniz")
-print("Lütfen aşağıya beden değerlerinizi giriniz...")
-
-a = int(input("Kilo : "))
-b = (float(input("Boy : ")))
-
-endex = a / b ** 2
-
-print("Beden Kitle İndeksiniz : {}".format(endex))
